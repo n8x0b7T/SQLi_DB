2494fc3515a1092dc54fe8b641b7c9bdac489e55
n8x0b7T <deretilium@protonmail.com>
2023-04-04 00:19:47 -0400

    authentication

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
+const crypto = require('crypto');
 
-var mysql = require('mysql')
+var mysql = require('mysql');
+const { hasSubscribers } = require('diagnostics_channel');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -12,10 +16,6 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
 app.get('/search', function (req, res) {
     connection.query(`SELECT * from ShopItem where itemName like "%${req.query.s}%"`, function (error, results, fields) {
         if (error) {
@@ -24,6 +24,51 @@ app.get('/search', function (req, res) {
         }
         res.send(results)
     });
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(`${__dirname}/index.html`);
+})
+
+app.get('/admin', function (req, res) {
+    connection.query(`SELECT * from ShopUser`, function (error, results, fields) {
+        if (error) {
+            res.status(500)
+            console.log(error)
+        }
+
+        if (req.cookies.password) {
+
+            hash = crypto.createHash('md5').update(req.cookies.password).digest('hex');
+        } else {
+            res.sendFile(`${__dirname}/admin.html`);
+        }
+
+        loggedIn = false
+        for (i = 0; i < results.length; i++) {
+
+            if (results[i].userName == req.cookies.username &&
+                results[i].userHash == hash) {
+                // res.sendFile(`${__dirname}/loggedin.html`);
+
+                loggedIn = true;
+            }
+
+        }
+
+        if (!loggedIn) {
+            res.sendFile(`${__dirname}/admin.html`);
+
+        } else {
+            res.sendFile(`${__dirname}/loggedin.html`);
+
+        }
+
+
+        console.log(req.cookies)
+    });
+
+
 })
 
 app.listen(3000)
